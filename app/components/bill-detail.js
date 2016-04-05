a9d464d1628601119d94bf3e8a5a9c97dd3eb4e7
Brad London and Michael Schennum <mikeschenphoto@gmail.com>
2016-04-05 11:52:41 -0700

    add name of sponsor to like of bills

--- app/components/bill-detail.js
+++ app/components/bill-detail.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullName: Ember.computed('bill.sponsor.title', 'bill.sponsor.first_name', 'bill.sponsor.last_name', function(){
+    return this.get('bill.sponsor.title') + '. ' + this.get('bill.sponsor.first_name') + ' ' + this.get('bill.sponsor.last_name');
+  }),
   addMoreInfo: false,
   actions: {
     infoFormShow() {
